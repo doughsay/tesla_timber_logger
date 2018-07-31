0da14c9bbf10801d58dce32baada7f962b9b3bbc
Chris Dosé <chris.dose@gmail.com>
2018-07-31 16:06:35 -0700

    Elixir 1.7 updates

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TeslaTimberLogger.MixProject do
     [
       app: :tesla_timber_logger,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
